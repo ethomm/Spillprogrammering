2ce01a3f05212d7dbe8dae0b39a5852e22d17315
Eirik Thommessen <eirik@meetprod.no>
2015-02-17 00:34:50 +0100

    Forsøk på noe greier

---
+++ aplha/Dont Pillage my Village/Assets/Scripts/Forsvarselement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Forsvarselement : MonoBehaviour {
+	//Lager variabler
+	public GameObject projektile;
+	public float reloadTime = 1f;
+	public float turnSpeed = 5f;
+	public float skytePause = .25f;
+	//Her kan det legges inn en effekt om man har noen
+	public Transform towerTarget;
+	public Transform skyteRettning;
+
+	private Quaternion bevegelsesRadius;
+	private float nesteSkudd = 0f;
+	private float nesteBevegelse = 0f;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (towerTarget)
+				if (Time.time >= nesteBevegelse)
+						CalculateAimPosition (towerTarget.position);
+						skyteRettning.rotation = Quaternion.Lerp (skyteRettning.rotation, bevegelsesRadius, Time.deltaTime * turnSpeed);
+				if(Time.time >=nesteSkudd)
+						SkytProsjektil();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Enemy")
+						nesteSkudd = Time.time + (reloadTime * .5f);
+						towerTarget = other.gameObject.transform;
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.gameObject.transform == towerTarget)
+						towerTarget = null;
+	}
+
+	void CalculateAimPosition(Vector3 targetPos){
+		//float siktepunkt = Vector3 (targetPos.x, targetPos.y, targetPos.z);
+		//bevegelsesRadius = Quaternion.LookRotation (siktepunkt);
+	}
+
+	void SkytProsjektil(){
+		nesteSkudd = Time.time + reloadTime;
+		nesteBevegelse = Time.time + skytePause;
+		Instantiate (prosjektil);
+	
+	}
+
+}
---
+++ aplha/Dont Pillage my Village/Assets/Scripts/prosjektil.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Lager en funksjon som beregner avstand
+public class prosjektil : MonoBehaviour {
+
+	//Setter farten på kula 
+	public float mySpeed = 10f;
+	//Setter maks avstand
+	public float myRange = 10f;
+	//Lager en tom variabel
+	private float myDist = 0f;
+
+	// Update is called once per frame
+	void Update () {
+		//Forteller hvilken vei og hastight kula skal gå, altså fremover
+		transform.Translate(Vector3.forward * Time.deltaTime * mySpeed);
+		//Beregner avstanden
+		myDist+= Time.deltaTime * mySpeed;
+		//Hvis avstanden er lik myRange, kullaforsvinner
+		if (myDist >= myRange)
+						Destroy (gameObject);
+		
+	}
+}
